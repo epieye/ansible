02c6cbd34d779123bac588707d63d32efc9442bc
Warre Matthews <warren@ourzoo.us>
2018-04-01 19:35:07 -0400

    create an ec2 instance

--- aws/aws_create.sh
+++ aws/aws_create.sh
@@ -1,31 +1,17 @@
-#aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type t2.micro --key-name MyKeyPair --security-group-ids sg-xxxxxxxx --subnet-id subnet-xxxxxxxx
-aws ec2 run-instances --image-id ami-26ebbc5c --count 1 --instance-type t2.micro --key-name Barry     --security-group-ids sg-3fa9bc49 --subnet-id subnet-cefe5de5
+require 'json'
+
+`aws ec2 run-instances --image-id ami-26ebbc5c --count 1 --instance-type t2.micro --key-name Maurice --security-group-ids sg-3fa9bc49 --subnet-id subnet-cefe5de5`
 
 
-#my vpcs:
-#vpc-705aa414
-#vpc-e7498e81
-#vpc-cb7a34ae <- default vpc
-#
-#security groups:
-#sg-3fa9bc49
-
-#apparently the public IP address isn't included in the response.
-# use: aws ec2 describe-instances
 
 
-#{
-#    "Reservations": [
-#        {
-#            "Instances": [
-#                {
-#                    "PublicDnsName": "ec2-52-91-210-80.compute-1.amazonaws.com", 
-#
+get_instance_id = JSON.parse(`aws ec2 describe-instances`)['Reservations'][0]['Instances'][0]['InstanceId']
+`aws ec2 create-tags --resources #{get_instance_id } --tags Key=Bob,Value=Your_Uncle`
 
 
-#{
-#    "Instances": [
-#        {
-#            "InstanceId": "i-02e5e15ae4e1fc2c2",
-#
-#aws ec2 create-tags --resources i- 02e5e15ae4e1fc2c2--tags Key=Bob,Value=Your_Uncle
+
+
+public_dns_name = JSON.parse(`aws ec2 describe-instances`)['Reservations'][0]['Instances'][0]['PublicDnsName']
+
+puts "ssh -i ~/.ssh/Maurice.pem ec2-user@#{public_dns_name}"
+
